dac0e2e5f91a7cde6311d5df41c2868ab2adf3de
hpaldan <42342816+hpaldan@users.noreply.github.com>
2021-04-24 12:15:39 +0200

    Update ReferenceGenerator.java
    Börjat lite smått på reference generatorn.

--- ReferenceGenerator.java
+++ ReferenceGenerator.java
@@ -1,4 +1,46 @@
 
-public class ReferenceGenerator {
+import javax.swing.JPanel;
 
+import se.lth.control.BoxPanel;
+import se.lth.control.MainFrame;
+
+public class ReferenceGenerator extends Thread {
+	private static final int MANUAL = 0, SQUARE = 1;
+	private final int priority;
+	private double amplitude;
+	private double period;
+	private double sign = -1;
+	private double manual;
+	
+	private class RefGUI{
+		private BoxPanel refPanel = new BoxPanel(BoxPanel.HORIZONTAL);
+		private JPanel sliderPanel = new JPanel();
+		private double amplitude;
+		
+		
+		
+		private double period; // Behövs denna?
+		
+		
+		//Constructor
+		private RefGUI(double amplitude,double period) {
+			MainFrame.showLoading();
+			this.amplitude = amplitude;
+			this.period = period;
+			
+			
+		}
+	}
+	
+	
+	//Constructor
+	public ReferenceGenerator(int refGenPriority) {
+		priority = refGenPriority;
+		amplitude = 5;
+		period = 15;
+		manual = 0.0;
+		
+		new RefGUI(amplitude,period);
+	}
+	
 }
